9b051f0652cf11ae757f0ad1d081b26540f4ae79
Joaquin dela Rosa <62501229+TheRealAnonymous008@users.noreply.github.com>
2023-07-30 09:50:43 +0800

    feat: fancy progress bars

--- Composer/train.py
+++ Composer/train.py
@@ -4,6 +4,7 @@ from torch.utils.data import DataLoader
 
 import torch
 import pandas as pd
+from tqdm import tqdm
 
 # PyTorch TensorBoard support
 from torch.utils.tensorboard import SummaryWriter
@@ -30,22 +31,28 @@ class TrainPipeline:
     def train_one_epoch(self, epoch_index, tb_writer):
         total_loss = 0
 
-        for i, batch in enumerate(self.train_loader):
-            self.optimizer.zero_grad()
-            
-            output_logits, notes_gt = self.unpack_batch(batch)
-            
-            loss = self.loss_fn(output_logits, notes_gt)
-            loss.backward() 
+        with tqdm(self.train_loader, unit="batch") as tepoch:
+            for i, batch in enumerate(tepoch):
+                tepoch.set_description(f"Epoch {epoch_index + 1}")
 
-            self.optimizer.step()
-            
-            # Gather data and report
-            total_loss += loss.item()
-            last_loss = loss.item()
-            print('  batch {} loss: {}'.format(i + 1, loss.item()))
-            tb_x = epoch_index * len(self.train_loader) + i + 1
-            tb_writer.add_scalar('Loss/train', last_loss, tb_x)
+                self.optimizer.zero_grad()
+                
+                output_logits, notes_gt = self.unpack_batch(batch)
+                
+                loss = self.loss_fn(output_logits, notes_gt)
+                loss.backward() 
+
+                self.optimizer.step()
+                
+                # Gather data and report
+                total_loss += loss.item()
+                last_loss = loss.item()
+
+                # Progress bar
+                tepoch.set_postfix({"train loss": f'{last_loss :.5f}'})
+                # Tensorboard scalars
+                tb_x = epoch_index * len(self.train_loader) + i + 1
+                tb_writer.add_scalar('Loss/train', last_loss, tb_x)
 
         return last_loss
     
@@ -53,13 +60,10 @@ class TrainPipeline:
         # Initializing in a separate cell so we can easily add more epochs to the same run
         timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
         writer = SummaryWriter('runs/composer_{}'.format(timestamp))
-        epoch_number = 0
 
         best_vloss = 1_000_000.
 
-        for epoch in range(epochs):
-            print('EPOCH {}:'.format(epoch_number + 1))
-
+        for epoch_number in range(epochs):
             # Make sure gradient tracking is on, and do a pass over the data
             self.model.train(True)
             avg_loss = self.train_one_epoch(epoch_number, writer)
@@ -71,13 +75,18 @@ class TrainPipeline:
 
             # Disable gradient computation and reduce memory consumption.
             with torch.no_grad():
-                for i, vdata in enumerate(self.validation_loader):
-                    voutputs, vgt = self.unpack_batch(vdata)
-                    vloss = self.loss_fn(voutputs, vgt)
-                    running_vloss += vloss
+                with tqdm(self.validation_loader, unit="batch") as tepoch:
+                    for i, vdata in enumerate(tepoch):
+                        tepoch.set_description(f"Epoch {epoch_number + 1}")
+                        voutputs, vgt = self.unpack_batch(vdata)
 
+                        vloss = self.loss_fn(voutputs, vgt)
+                        running_vloss += vloss.item()
+                        avg_vloss = running_vloss / (i + 1)
+
+                        tepoch.set_postfix({"val loss": f'{avg_vloss:.5f}'})
+            
             avg_vloss = running_vloss / (i + 1)
-            print('LOSS train {} valid {}'.format(avg_loss, avg_vloss))
 
             # Log the running loss averaged per batch
             # for both training and validation
